83da05209592fb76a59ff61c43e3a23fe52e4ee5
Oliver Earl <oliver@oliverearl.co.uk>
2025-10-08 19:23:32 +0100

    Start to remove old chunks of the old codebase
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- src/helpers.php
+++ src/helpers.php
@@ -1,18 +1,3 @@
 <?php
 
 declare(strict_types=1);
-
-use Petrobolos\FixedArray\FixedArrayable;
-
-if (! function_exists('fixedArray')) {
-    /**
-     * Create a new fixed array fluent interface.
-     *
-     * @param mixed|null $input
-     * @return \Petrobolos\FixedArray\FixedArrayable
-     */
-    function fixedArray(mixed $input = null): FixedArrayable
-    {
-        return new FixedArrayable($input);
-    }
-}
