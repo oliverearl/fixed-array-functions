396b7345bacfb04fa6f14fd51805a684314172e1
Oliver Earl <oliver@oliverearl.co.uk>
2025-10-08 20:28:29 +0100

    Pint
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- tests/Unit/Facades/FixedArrayTest.php
+++ tests/Unit/Facades/FixedArrayTest.php
@@ -6,7 +6,7 @@ use Petrobolos\FixedArray\Facades\FixedArray;
 use Petrobolos\FixedArray\FixedArray as BaseFixedArray;
 
 it('can return a facade accessor', function (): void {
-    $facade = new class () extends FixedArray {
+    $facade = new class extends FixedArray {
         /**
          * Expose the protected method for testing.
          */
