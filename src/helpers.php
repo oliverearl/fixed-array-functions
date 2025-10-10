964e56471a272b85a9f887b242ab3eb4186e456d
Oliver Earl <oliver@oliverearl.co.uk>
2025-10-10 20:26:23 +0100

    Add helper function to create a new FixedArrayable instance
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- src/helpers.php
+++ src/helpers.php
@@ -1,3 +1,15 @@
 <?php
 
 declare(strict_types=1);
+
+use Petrobolos\FixedArray\Fluent\FixedArrayable;
+
+if (! function_exists('fixedArray')) {
+    /**
+     * Create a new FixedArrayable instance.
+     */
+    function fixedArray(mixed $value): FixedArrayable
+    {
+        return FixedArrayable::make($value);
+    }
+}
