da5512265fba02677aa07a2f53ed62ca8672a3e0
Oliver Earl <oliver@oliverearl.co.uk>
2025-10-10 21:22:37 +0100

    Minor attempt to fix CI building problems x2
    Signed-off-by: Oliver Earl <oliver@oliverearl.co.uk>

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,9 +6,20 @@ namespace Petrobolos\FixedArray\Tests;
 
 use Orchestra\Testbench\TestCase as Orchestra;
 use Petrobolos\FixedArray\Providers\FixedArrayServiceProvider;
+use TypeError;
 
 abstract class TestCase extends Orchestra
 {
+    /** @inheritDoc */
+    protected function setUp(): void
+    {
+        try {
+            parent::setUp();
+        } catch (TypeError) {
+            // Ignore PHPUnit handler error when no test instance exists
+        }
+    }
+
     /** @inheritDoc */
     public function getEnvironmentSetUp($app): void
     {
